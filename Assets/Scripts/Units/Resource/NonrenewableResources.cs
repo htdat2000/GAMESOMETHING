cc86784f76365a9b4e2731a371442923e9df523d
htdat2000 <12a6.ht.dat@gmail.com>
2022-01-18 19:14:27 +0700

    ImportPackage

---
+++ Assets/Scripts/Units/Resource/NonrenewableResources.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class NonrenewableResources : Resource 
+{
+    protected int defaultHP;
+    [SerializeField] protected Sprite havingMaterialImg;
+    void Start()
+    {   
+        defaultHP = hp;
+        currentMaterialHolding = maxMaterialCanHold;
+
+        itemPrototype = UnityEngine.Resources.Load<GameObject>("Prefabs/Items/ItemPrototype");
+        itemPrototype.GetComponent<ItemPrototype>().item = materialsHolding;
+    }
+    protected override void SpawnMaterials()
+    {
+        currentMaterialHolding--;
+        Vector2 spawnPosition = new Vector2(transform.position.x + Random.Range(-0.5f, 0.5f), transform.position.y + Random.Range(-0.5f, 0.5f));
+        Instantiate(itemPrototype, spawnPosition, Quaternion.identity);
+        if(currentMaterialHolding <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public override void Remove()
+    {
+        return;
+    }
+    public override void Spawn()
+    {
+        return;
+    }
+    protected override void TakeDmg(int dmg)
+    {
+        hp -= dmg;
+        HPEqual0();
+    }
+    protected override void HPEqual0()
+    {
+        if(hp <= 0)
+        {
+            SpawnMaterials();
+            hp = defaultHP;
+        }
+    }
+}
---
+++ Assets/Scripts/Units/Resource/RenewableResources.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+public class RenewableResources : Resource
+{
+    protected int defaultHP;
+    [SerializeField] protected Sprite havingMaterialImg;
+    [SerializeField] protected Sprite notHavingMaterialImg;
+    protected int renewTime;
+    protected float cooldown; 
+    protected SpriteRenderer spriteRenderer;  
+    void Start()
+    {   
+        defaultHP = hp;
+        cooldown = renewTime;
+        currentMaterialHolding = maxMaterialCanHold;
+        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
+        itemPrototype = UnityEngine.Resources.Load<GameObject>("Prefabs/Items/ItemPrototype");
+        itemPrototype.GetComponent<ItemPrototype>().item = materialsHolding;
+        
+        InvokeRepeating("UpdateResourceStatus", 0, 0.5f);
+    }
+    void Update()
+    {
+        GainMaterials();
+    }
+    protected override void SpawnMaterials()
+    {   
+        Vector2 spawnPosition = new Vector2(transform.position.x + Random.Range(-0.5f, 0.5f), transform.position.y + Random.Range(-0.5f, 0.5f));
+        Instantiate(itemPrototype, spawnPosition, Quaternion.identity);
+        currentMaterialHolding--;
+    }
+    protected virtual void GainMaterials()
+    {
+        if(currentMaterialHolding < maxMaterialCanHold)
+        {
+            if(cooldown <= 0)
+            {
+                currentMaterialHolding++;
+                cooldown = renewTime;
+            }
+            else
+                cooldown -= Time.deltaTime; 
+        }
+    }
+    protected virtual void UpdateResourceStatus()
+    {
+        if(currentMaterialHolding <= 0)
+        {
+            spriteRenderer.sprite = notHavingMaterialImg;
+        }
+        else spriteRenderer.sprite = havingMaterialImg;
+    }
+    public override void Remove()
+    {
+        return;
+    }
+    public override void Spawn()
+    {
+        return;
+    }
+    protected override void TakeDmg(int dmg)
+    {
+        if(currentMaterialHolding > 0)
+        {
+        hp -= dmg;
+        HPEqual0();
+        }
+    }
+    protected override void HPEqual0()
+    {
+        if(hp <= 0)
+        {
+            SpawnMaterials();
+            hp = defaultHP;
+        }
+    }
+}
