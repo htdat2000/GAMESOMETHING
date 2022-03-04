6f860cefadda7f5ef077375ce24ae2bd549a2306
htdat2000 <94451439+htdat2000@users.noreply.github.com>
2022-03-04 18:50:08 +0700

    CombatSystem (#21)
    * PlayerAttackEnemies
    
    * EnemiesAttackPlayer

--- Assets/Scripts/Units/Bases/DamageableObjects.cs
+++ Assets/Scripts/Units/Bases/DamageableObjects.cs
@@ -5,8 +5,8 @@ public abstract class DamageableObjects : MonoBehaviour
     [SerializeField] protected int _id;
     [SerializeField] protected string _name;
     [Header("Attributes")]
-    [SerializeField] protected int hp; 
-    [SerializeField] public int dmg { get; }
+    protected int hp; 
+    protected int _dmg; public int dmg { get { return _dmg; } set { _dmg = value; } }
 
     public abstract void TakeDmg(int dmg);
     protected abstract void HPEqual0();
--- Assets/Scripts/Units/Bases/Mobs.cs
+++ Assets/Scripts/Units/Bases/Mobs.cs
@@ -24,13 +24,15 @@ public class ItemDrop
 public abstract class Mobs : Creatures, IAutoSpawn
 {
     [SerializeField] protected ItemDrop[] itemDrops;
-    protected int defaultHP;
+    [SerializeField] protected int defaultHP;
+    [SerializeField] protected int defaultDmg;
     protected GameObject target;
     protected GameObject itemPrototype;
-
+    
     protected virtual void Start()
     {
         itemPrototype = UnityEngine.Resources.Load<GameObject>("Prefabs/Items/ItemPrototype");
+        LoadParameter();
     }
     public void Remove()
     {
@@ -47,6 +49,7 @@ public abstract class Mobs : Creatures, IAutoSpawn
 
     public override void TakeDmg(int dmg)
     {
+        Debug.Log("Take dmg");
         hp -= dmg;
         hp = Mathf.Clamp(hp, 0, defaultHP);
         HPEqual0();
@@ -69,6 +72,10 @@ public abstract class Mobs : Creatures, IAutoSpawn
     protected void DropItem()
     {
         int randomValue = Random.Range(1, 1001);
+        if(itemDrops.Length <= 0)
+        {
+            return;
+        }
         foreach (ItemDrop item in itemDrops)
         {
             if(item.SpawnItemByDropRate(randomValue))
@@ -81,5 +88,11 @@ public abstract class Mobs : Creatures, IAutoSpawn
     protected void SpawnItem(Items item)
     {
         itemPrototype.GetComponent<ItemPrototype>().item = item;
+    }
+
+    protected void LoadParameter()
+    {
+        hp = defaultHP;
+        dmg = defaultDmg;
     }
 }
