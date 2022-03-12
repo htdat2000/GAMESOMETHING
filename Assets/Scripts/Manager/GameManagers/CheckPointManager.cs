0ad19b7f368f390a6cbe458068425cd80aa1b44a
htdat2000 <12a6.ht.dat@gmail.com>
2022-03-12 19:54:58 +0700

    DieReturnCheckPoint

---
+++ Assets/Scripts/Manager/GameManagers/CheckPointManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointManager : MonoBehaviour
+{   
+    public static CheckPointManager instance;
+    public GameObject checkPoint;
+
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.LogError("More than 1 CheckPointManager in scene");
+        }
+    }
+
+    public void RespawnPlayer(GameObject player)
+    {
+        player.transform.position = checkPoint.transform.position;
+    }
+}
