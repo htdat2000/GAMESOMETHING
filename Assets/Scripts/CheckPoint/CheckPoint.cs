f15b0dd3dcfe266a617d03e6ef69c9e8326b371c
htdat2000 <12a6.ht.dat@gmail.com>
2022-03-13 09:16:46 +0700

    CheckPointVer2.0(CodeDemo)

---
+++ Assets/Scripts/CheckPoint/CheckPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public int checkPointID;
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            SetActiveCheckPoint();
+        }
+    }
+
+    private void SetActiveCheckPoint()
+    {
+        CheckPointManager.instance.ActiveCheckPoint(checkPointID);
+    }
+}
