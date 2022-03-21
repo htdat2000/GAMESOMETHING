53ee8325624bbba23ecad20ff7420f0b14a90a93
htdat2000 <94451439+htdat2000@users.noreply.github.com>
2022-03-21 19:18:35 +0700

    InteractCampfire (#49)
    * AddCampfire
    
    * InteractCampfire
    
    * SettingTime

---
+++ Assets/Scripts/Units/Buildings/Campfire.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Campfire : Buildings
+{
+    public override void Interact()
+    {
+        Debug.Log("interact campfire");
+    }
+
+    protected override void Broken()
+    {
+        return;
+    }
+
+    public override void TakeDmg(int dmg)
+    {
+        return;
+    }
+    protected override void HPEqual0()
+    {
+        return;
+    }
+}
