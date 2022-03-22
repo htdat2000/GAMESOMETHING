2d83bcf73185a22eb10d2da63101c7b0623cf4f1
Mực <83195998+Muc2k1@users.noreply.github.com>
2022-03-22 08:46:02 +0700

    *Improve BagUI

--- Assets/Scripts/Units/Mobs/Enemies/Boar.cs
+++ Assets/Scripts/Units/Mobs/Enemies/Boar.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Boar : Enemies
 {
     private Vector2 randomPoint;
+    
+    protected override void Start() 
+    {
+        base.Start();
+        InvokeRepeating("ChoseRandomDir", 0f, 3f);
+    }
     protected override void Update() 
     {
         base.Update();
@@ -14,5 +20,11 @@ public class Boar : Enemies
     {
         transform.position += (transform.position - (Vector3)randomPoint).normalized * speed * 0.1f * Time.deltaTime;
         SetAnim("Idle");
+    }
+    private void ChoseRandomDir()
+    {
+        float randx = Random.Range(transform.position.x - 1f,transform.position.x + 1f);
+        float randy = Random.Range(transform.position.y - 1f,transform.position.y + 1f);
+        randomPoint = new Vector2(randx, randy);
     }
 }
