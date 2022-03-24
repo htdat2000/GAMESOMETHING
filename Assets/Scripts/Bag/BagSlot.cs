ae63960090837ff8ab0d37164c8c7be2a06c6de1
htdat2000 <12a6.ht.dat@gmail.com>
2022-03-24 20:01:03 +0700

    MultiLanguageForTooltip

--- Assets/Scripts/Bag/BagSlot.cs
+++ Assets/Scripts/Bag/BagSlot.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class BagSlot : MonoBehaviour
+public class BagSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [Header("Item Area")]
     public Items item;
@@ -79,6 +80,18 @@ public class BagSlot : MonoBehaviour
     void ResetTapCount()
     {
         tapCount = 0;
+    }
+    #endregion
+
+    #region Tooltip
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        TooltipSystem.tooltipSystem.Show("Items_Description", item.name_);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        TooltipSystem.tooltipSystem.Hide();
     }
     #endregion
 }
---
+++ Assets/Scripts/TooltipSystem/TooltipSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Localization.Components;
+
+public class TooltipSystem : MonoBehaviour
+{
+    public static TooltipSystem tooltipSystem;
+
+    [Header("Tooltip UI")]
+    [SerializeField] TextMeshProUGUI content;
+    [SerializeField] LocalizeStringEvent contentKey;
+
+    void Start()
+    {
+        if(tooltipSystem != null)
+        {
+            Debug.LogError("More than 1 Tooltip System in scene");
+            return;
+        }
+        tooltipSystem = this;
+    }
+
+    public void Show(string table, string key)
+    {
+        //contentKey.Localization.TableReference = "Item_Description";
+        //contentKey.LocalizedString.TableEntryReference = key;
+        contentKey.StringReference.TableReference = table;
+        contentKey.StringReference.TableEntryReference = key;
+        content.gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        content.gameObject.SetActive(false);
+    }
+
+
+
+
+}
