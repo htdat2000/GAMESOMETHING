1122037f9a98feba63fff1bef8b3334483b12f9d
htdat2000 <12a6.ht.dat@gmail.com>
2022-03-30 09:39:57 +0700

    AddSFXManager

---
+++ Assets/Scripts/Scriptables/Audios/SFX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "newSFX", menuName = "Sound/SFX")]
+public class SFX : Sound
+{
+    [System.Serializable]
+    public class SFXType
+    {
+        public SFXState type;
+        public AudioClip clip;
+    }
+
+    public enum SFXState
+    {
+        HurtSFX,
+        AttackSFX,
+        DieSFX
+    }
+    public SFXType[] types;
+
+    public void PlaySFX(SFXState state)
+    {
+        AudioClip _audioClip = Array.Find(types, type => type.type == state).clip;
+        SFXManager.sfxManager.PlaySFX(_audioClip);
+    }
+}
--- Assets/Scripts/Scriptables/Audios/Sound.cs
+++ Assets/Scripts/Scriptables/Audios/Sound.cs
@@ -6,5 +6,4 @@ public class Sound : ScriptableObject
     public string name;
     public AudioClip audioClip;
     [Range(0, 1)] public float volume;
-    [HideInInspector] public AudioSource audioSource;
 }
