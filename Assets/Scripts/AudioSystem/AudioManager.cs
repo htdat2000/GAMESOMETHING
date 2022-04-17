aff3898e3189868a769a2de3fba24e7de0d962ba
htdat2000 <12a6.ht.dat@gmail.com>
2022-04-17 19:28:16 +0700

    SoundIconChange

--- Assets/Scripts/AudioSystem/AudioManager.cs
+++ Assets/Scripts/AudioSystem/AudioManager.cs
@@ -1,20 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 public class AudioManager : MonoBehaviour
 {
+    [Header("Music")]
+    [SerializeField] Image musicImg;
+    [SerializeField] Sprite musicIcon;
+    [SerializeField] Sprite muteMusicIcon;
     [SerializeField] protected MusicManager musicManager;
-    [SerializeField] protected SFXManager sfxManager; 
 
+    [Header("SFX")]
+    [SerializeField] Image sfxImg;
+    [SerializeField] Sprite sfxIcon;
+    [SerializeField] Sprite muteSFXIcon;
+    [SerializeField] protected SFXManager sfxManager;
+     
     public void MusicSwitch()
     {
+        if(musicManager.gameObject.activeSelf)
+        {
+            musicImg.sprite = muteMusicIcon;
+        }
+        else
+        {
+            musicImg.sprite = musicIcon;
+        }
         musicManager.gameObject.SetActive(!musicManager.gameObject.activeSelf);
     }
 
     public void SFXSwitch()
     {
+        if(sfxManager.gameObject.activeSelf)
+        {
+            sfxImg.sprite = muteSFXIcon;
+        }
+        else
+        {
+            sfxImg.sprite = sfxIcon;
+        }
         sfxManager.gameObject.SetActive(!sfxManager.gameObject.activeSelf);
     }
 }
